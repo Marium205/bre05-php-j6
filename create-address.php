aec36aa8c3de67fe59067f07c7b933e34547ff5e
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-02-18 14:52:22 +0000

    étape 6 : create address

---
+++ create-address.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'connexion.php';
+
+$street = $_POST['street'];
+$city = $_POST['city'];
+$zipcode = $_POST['zipcode'];
+
+$query = $db->prepare('INSERT INTO address (street, city, zipcode) VALUES (:street, :city, :zipcode)');
+
+$parameters = [
+    'street' => $street,
+    'city' => $city,
+    'zipcode' => $zipcode
+];
+
+$query->execute($parameters);
+
+echo "Adresse créée";
+
+?>
