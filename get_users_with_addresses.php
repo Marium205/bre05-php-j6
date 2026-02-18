bff89aaafbcc23b8d68b58ac0cdbe091b6cb14f2
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-02-18 13:31:20 +0000

    étape 5 : get users with addresses

---
+++ get_users_with_addresses.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'connexion.php';
+
+$query = $db->prepare('SELECT * FROM users JOIN address ON users.address = address.id');
+
+$query->execute();
+
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($users);
+
+?>
