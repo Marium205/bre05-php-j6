3838cce74f2827405a283bf74e6c9047dc7d5398
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-02-18 09:51:19 +0000

    Etape 1: connexion

--- connexion.php
+++ connexion.php
@@ -1,4 +1,18 @@
-User : mariyamabdulrauf
-Password : e832cf62941e3fa2da0f3adea80ed631
-Host : db.3wa.io
-Le nom de vos bases de données doit commencer comme suit : mariyamabdulrauf_
+<?php
+
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "mariyamabdulrauf_phpj6";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "mariyamabdulrauf_";
+$password = "e832cf62941e3fa2da0f3adea80ed631";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+var_dump($db);
+?>
